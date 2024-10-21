51800e2b8b17123d756f25e13ef6ddb17a81658c
salandr <ansaliy200013@gmail.com>
2024-10-21 12:36:47 +0100

    HW1 Done

---
+++ App.js
@@ -0,0 +1,56 @@
+import RegistrationScreen from "./screens/RegistrationScreen";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import bgImage from "./assets/bgd.jpg";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import LoginScreen from "./screens/LoginScreen";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Light": require("./assets/fonts/Roboto-Light.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1 }}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  return (
+    <>
+      {/* <LoginScreen /> */}
+      <RegistrationScreen />
+    </>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+
+//   text: {
+//     fontFamily: "Roboto-Bold",
+//   },
+//   // bg: {
+//   //   flex: 1,
+//   //   justifyContent: "center",
+//   // },
+// });
